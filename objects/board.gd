81b1e8e6b3dbc7c7b6937d9de800ec7b9ebc2b33
Guille <guillermoprados@users.noreply.github.com>
2023-09-21 10:39:26 -0300

    updated the board

--- objects/board.gd
+++ objects/board.gd
@@ -4,6 +4,7 @@ extends Node2D
 
 @export var cell_scene: PackedScene
 
+var size : Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if levelConfig:
@@ -16,19 +17,14 @@ func _process(delta):
 
 
 func createBoard(rows: int, columns: int):
+	var cell_size:Vector2
 	for row in range(rows):
 		for col in range(columns):
 			var cell_instance = cell_scene.instantiate()
-			var cell_size:Vector2 = cell_instance.size()
+			cell_size = cell_instance.size()
 			cell_instance.position = Vector2(col * cell_size.x, row * cell_size.y)
 			cell_instance.cell_index = Vector2(row, col)
 			# Add the cell instance as a child of the board scene
 			add_child(cell_instance)
-
-			# You can perform any additional initialization or configuration of the cell instance here
-
-			# Optionally, store a reference to the cell instance if needed
-			# cell_instances[row][col] = cell_instance
-
-# Assuming you have cell_width and cell_height defined somewhere
+	size = Vector2(columns * cell_size.x, rows * cell_size.y)
 
