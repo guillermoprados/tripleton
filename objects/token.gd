9b876afad5e478c920fa68bbdccb33cbdc7f0790
Guille <guillermoprados@users.noreply.github.com>
2023-09-21 21:32:28 -0300

    start adding tokens methods to board

---
+++ objects/token.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var floating : bool
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
--- scenes/gameplay.gd
+++ scenes/gameplay.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
+@export var token_scene: PackedScene
 
 func _ready():
 	# Connect the screen size changed signal to a function
 	get_tree().root.size_changed.connect(_on_screen_size_changed)
 	_on_screen_size_changed()
+	create_floating_token()
 
 func _on_screen_size_changed():
 	var screen_size = get_viewport().get_visible_rect().size
@@ -15,3 +17,11 @@ func _on_screen_size_changed():
 		(screen_size.y - board_size.y) / 2
 	)
 	$Board.position = board_pos
+
+func create_floating_token():
+	var token_instance = token_scene.instantiate()
+		# cell_size = cell_instance.size()
+		# cell_instance.position = Vector2(col * cell_size.x, row * cell_size.y)
+		# cell_instance.cell_index = Vector2(row, col)
+		# Add the cell instance as a child of the board scene
+	add_child(token_instance)
