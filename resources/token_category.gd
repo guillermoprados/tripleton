c1135c1bed4a8ddf8ce7077abd4986264effd0f7
Guille <guillermoprados@users.noreply.github.com>
2023-09-23 23:48:28 -0300

    start adding categories

---
+++ resources/token_category.gd
@@ -0,0 +1,7 @@
+extends Resource
+
+class_name TokenCategory
+
+@export var name: String
+@export var ordered_tokens: Array[TokenData]
+
--- resources/tokens_array.gd
+++
@@ -1,5 +0,0 @@
-extends Resource
-
-class_name TokensDataArray
-
-@export var tokens : Array[TokenData] = []
