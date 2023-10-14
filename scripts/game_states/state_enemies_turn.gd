0df1bb0af747e41d2962cec3d7044fdada3f797a
Guille <guillermoprados@users.noreply.github.com>
2023-10-14 16:11:56 -0300

    check when enemies are enclosed with friends

--- scripts/game_states/state_enemies_turn.gd
+++ scripts/game_states/state_enemies_turn.gd
@@ -35,18 +35,100 @@ func _on_stucked_enemy(cell_index:Vector2) -> void:
 	stucked_enemies.append(cell_index)
 	
 func check_dead_enemies() -> void:
+	var simplified_board_info = __convert_board_to_array(board)
+	
 	for cell_index in stucked_enemies:
-		var enemy_token: Token = board.get_token_at_cell(cell_index)
-		var next_token_data: TokenData = enemy_token.data.next_token
-		var grave_token:Token = game_manager.instantiate_new_token(next_token_data, cell_index, null)
-		board.clear_token(cell_index)
-		board.set_token_at_cell(grave_token, cell_index)
+		if not __can_reach_empty_cell(cell_index, simplified_board_info):
+			var enemy_token: Token = board.get_token_at_cell(cell_index)
+			var next_token_data: TokenData = enemy_token.data.next_token
+			var grave_token:Token = game_manager.instantiate_new_token(next_token_data, cell_index, null)
+			board.clear_token(cell_index)
+			board.set_token_at_cell(grave_token, cell_index)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta:float) -> void:
-	if number_of_pending_actions == 0:
+	if stucked_enemies.size() > 0:
 		check_dead_enemies()
+	if number_of_pending_actions == 0:
 		finish_enemies_turn()
 	
 func finish_enemies_turn() -> void:
 	state_finished.emit(id)
+
+enum CellType {
+	EMPTY,
+	ENEMY,
+	OTHER
+}
+
+func __can_reach_empty_cell(start_pos: Vector2, board: Array) -> bool:
+	var directions = [
+		Vector2(0, 1),  # Down
+		Vector2(1, 0),  # Right
+		Vector2(0, -1), # Up
+		Vector2(-1, 0)  # Left
+	]
+	
+	var visited = {}  # A Dictionary to keep track of visited cells
+	var queue = []    # An Array to use as a queue for BFS
+	
+	# Start BFS from the starting position
+	queue.append(start_pos)
+	visited[start_pos] = true
+	
+	while queue.size() > 0:
+		var current = queue.pop_front()
+		
+		# Check for each possible direction
+		for dir in directions:
+			var next_pos = current + dir
+			
+			# Check for out-of-bounds
+			if next_pos.x < 0 or next_pos.x >= board.size() or next_pos.y < 0 or next_pos.y >= board[0].size():
+				continue
+			
+			# Check if the next position was visited before
+			if visited.has(next_pos):
+				continue
+				
+			# Mark the position as visited
+			visited[next_pos] = true
+			
+			# Check the type of cell at next_pos
+			if board[next_pos.x][next_pos.y] == CellType.EMPTY:
+				# Found an empty cell
+				return true
+			elif board[next_pos.x][next_pos.y] == CellType.ENEMY:
+				# Add enemy cell to the queue for further exploration
+				queue.append(next_pos)
+	
+	# No path found to an empty cell
+	return false
+
+func __convert_board_to_array(board: Board) -> Array:
+	var converted_board = []
+	
+	# Iterating through all cells in the board
+	for i in range(board.rows):
+		var row = []
+		
+		for j in range(board.columns):
+			var cell_index = Vector2(i, j)
+			
+			# Check if the cell is empty
+			if board.is_cell_empty(cell_index):
+				row.append(CellType.EMPTY)
+				continue
+			
+			# Check if there's a token at the cell
+			var token = board.get_token_at_cell(cell_index)
+			
+			if token and token.type == Constants.TokenType.ENEMY:
+				row.append(CellType.ENEMY)
+			else:
+				row.append(CellType.OTHER)
+		
+		# Append the row to the converted_board
+		converted_board.append(row)
+	
+	return converted_board
