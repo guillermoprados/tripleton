053c350da46052af8e3b1019bc5aef97558234bf
Guille <guillermoprados@users.noreply.github.com>
2023-10-22 20:15:07 -0300

    use file name as id in tokens

--- resources/token_data.gd
+++ resources/token_data.gd
@@ -2,8 +2,19 @@ extends Resource
 
 class_name TokenData
 
-@export var id:String
+var _name:String
 
+var id:String:
+	get:
+		if not _name:
+			_name = get_name_from_path(get_path())
+		return _name
+
+func get_name_from_path(path: String) -> String:
+	var filename = path.get_file()
+	var name = filename.get_basename()
+	return name
+	
 @export var sprite_scene: PackedScene
 
 func type() -> Constants.TokenType:
