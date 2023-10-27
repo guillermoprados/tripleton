624753d0b7e824c57de8bd31d4f7fa41a5515fda
Guille <guillermoprados@users.noreply.github.com>
2023-10-27 18:36:30 -0300

    added an script to export the files

---
+++ bash/export_sprites.sh
@@ -0,0 +1,11 @@
+source ~/.zshrc
+
+#!/bin/bash
+
+# Get the directory where the script is located
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "$script_dir"
+
+#export token images
+aseprite -b ../asesprite/tokens.aseprite --save-as ../images/tokens/{slice}.png
