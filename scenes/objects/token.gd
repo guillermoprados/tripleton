367a8218184e8f32fbb7e01e510aeb38e1c3750a
Guille <guillermoprados@users.noreply.github.com>
2023-10-27 19:46:29 -0300

    added floating token shader

--- scenes/objects/token.gd
+++ scenes/objects/token.gd
@@ -6,9 +6,11 @@ class_name Token
 @export var color_highlight_invalid : Color = Color(1, 0.5, 0.5, 1)
 @export var color_highlight_transparent : Color = Color(1, 1, 1, 0.5)
 
+@export var outline_shader:ShaderMaterial
 @export var sprite_holder:Node2D
 @export var shadow:Node2D
 
+var original_material:ShaderMaterial
 
 var _floating: bool
 
@@ -20,10 +22,14 @@ var floating: bool:
 		if _floating:
 			sprite_holder.position.y = Constants.TOKEN_FLOATING_Y_POS
 			shadow.scale = Vector2(Constants.TOKEN_SHADOW_FLOATING_MULTIPLIER, Constants.TOKEN_SHADOW_FLOATING_MULTIPLIER)
+			sprite.material = outline_shader
+			sprite.get_material().set_shader_parameter("line_color", Color(1.0, 1.0, 1.0))
+			sprite.get_material().set_shader_parameter("line_thickness", 4.0)  # Adjust the size as needed
 		else:
 			sprite_holder.position.y = Constants.TOKEN_PLACED_Y_POS
 			shadow.scale = Vector2(1, 1)
-
+			sprite.material = original_material
+			
 var id:String:
 	get:
 		return data.id
@@ -58,6 +64,7 @@ func set_data(token_data:TokenData) -> void:
 	data = token_data
 	
 	sprite = token_data.sprite_scene.instantiate()
+	original_material = sprite.material
 	sprite_holder.add_child(sprite)
 	sprite_holder.position.x = Constants.CELL_SIZE.x / 2
 	sprite_holder.position.y = Constants.TOKEN_PLACED_Y_POS
