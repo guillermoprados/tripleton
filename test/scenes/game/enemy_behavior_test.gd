2d3c50638218f0e396774b09ef9cf13c98393c29
Guille <guillermoprados@users.noreply.github.com>
2023-11-13 20:37:09 -0300

    change tests structure

---
+++ test/scenes/game/enemy_behavior_test.gd
@@ -0,0 +1,9 @@
+# GdUnit generated TestSuite
+class_name EnemyBehabiorTest
+extends GameManagerTest
+@warning_ignore('unused_parameter')
+@warning_ignore('return_value_discarded')
+
+
+func test__try_multi_level_combination() -> void:
+	pass
