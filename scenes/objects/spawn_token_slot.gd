783e6be3bc729dfbbbd38933ff5a1a21036ae734
Guille <guillermoprados@users.noreply.github.com>
2023-11-23 10:08:41 -0300

    add back token

--- scenes/objects/spawn_token_slot.gd
+++ scenes/objects/spawn_token_slot.gd
@@ -12,13 +12,13 @@ signal on_slot_selected(index:int)
 @export var cell_board:BoardCell
 @export var background:Sprite2D
 
-var __spawned_token:BoardToken
-var spawned_token:BoardToken:
+var __token:BoardToken
+var token:BoardToken:
 	get:
-		return __spawned_token
+		return __token
 
 var __ghost_token:BoardToken
-var gosth_back_token:BoardToken:
+var back_token:BoardToken:
 	get:
 		return __ghost_token
 		
@@ -30,36 +30,47 @@ func _process(delta):
 	pass
 
 func spawn_new_random_token(difficulty:Difficulty) -> void:
-	assert(not __spawned_token, "trying to spawn a token when there is already one")
+	assert(not __token, "trying to spawn a token when there is already one")
 	spawn_new_token(difficulty.get_random_token_data())
 	
 func spawn_new_token(token_data:TokenData) -> void:
-	assert(not __spawned_token, "trying to spawn a token when there is already one")
+	assert(not __token, "trying to spawn a token when there is already one")
 	var new_token := token_scene.instantiate() as BoardToken
-	new_token.set_data(token_data, Constants.TokenStatus.BOXED)
+	new_token.set_data(token_data, Constants.TokenStatus.NONE)
+	new_token.z_as_relative = false
 	box_token(new_token, false)
 
 func discard_token() -> void:
-	assert(__spawned_token, "cannot discard token if there isn't one")
-	remove_child(__spawned_token)
-	__spawned_token.queue_free()
-	__spawned_token = null
+	assert(__token, "cannot discard token if there isn't one")
+	remove_child(__token)
+	__token.queue_free()
+	__token = null
+	remove_child(__ghost_token)
+	__ghost_token.queue_free()
+	__ghost_token = null
 
 func box_token(to_box_token:BoardToken, animated:bool = false) -> void:
-	assert(not __spawned_token, "trying to spawn a token when there is already one")
-	__spawned_token = to_box_token	
-	add_child(__spawned_token)
-	spawned_token.z_index = Constants.TOKEN_BOXED_Z_INDEX
+	assert(not __token, "trying to box a token when there is already one")
+	__token = to_box_token	
+	add_child(__token)
+	token.set_status(Constants.TokenStatus.BOXED)
+	token.z_index = Constants.TOKEN_BOXED_Z_INDEX
+	
+	__set_boxed_token_back()
+	
 	if animated:
 		var tween = create_tween()
 		tween.set_ease(Tween.EASE_IN)
-		tween.tween_property(spawned_token, "position", self.position, 0.2)	
+		tween.tween_property(token, "position", self.position, 0.2)	
 	else:
-		spawned_token.position = Vector2.ZERO
+		token.position = Vector2.ZERO
 	
-func __set_back_token_for_token(token:BoardToken) -> void:
-	var new_token := token_scene.instantiate() as BoardToken
-	#__back_token = instantiate_new_token(token_data, Constants.TokenStatus.GHOST_BOX)
-	#add_child(ghost_token)
-	#ghost_token.position = spawn_token_cell.position
-	#ghost_token.z_index = Constants.GHOST_BOX_Z_INDEX
+func __set_boxed_token_back() -> void:
+	
+	if not back_token:
+		__ghost_token = token_scene.instantiate() as BoardToken
+		add_child(__ghost_token)
+		back_token.z_index = Constants.GHOST_BOX_Z_INDEX
+		back_token.z_as_relative = false
+	
+	back_token.set_data(token.data, Constants.TokenStatus.GHOST_BOX)
