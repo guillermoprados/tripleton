a14aa54f6fae93184c1c13e3eb00c697e3f0a6a7
Guille <guillermoprados@users.noreply.github.com>
2023-11-27 19:27:48 -0300

    wip

--- test/scenes/game/difficulties_test.gd
+++ test/scenes/game/difficulties_test.gd
@@ -51,3 +51,68 @@ func test__when_points_excedded_diff_max_points_on_last_diff_it_should_stay() ->
 	diff_points = game_manager.difficulty.total_points
 	game_manager.add_points(diff_points + 10)
 	assert_str(game_manager.difficulty.name).is_equal("Hard")
+
+
+func test__easy_diff_should_use_default_chest_bronze() -> void:
+	
+	
+	var landscape := [
+		[IDs.EMPTY,IDs.EMPTY,IDs.EMPTY],
+		[IDs.EMPTY,IDs.EMPTY,IDs.EMPTY],
+		[IDs.EMPTY,IDs.TREEE,IDs.EMPTY],
+		[IDs.EMPTY,IDs.TREEE,IDs.EMPTY]
+	]
+	
+	await __set_to_player_state_with_board(landscape)
+	
+	assert_str(game_manager.difficulty.name).is_equal("Easy")
+	assert_int(game_manager.difficulty.max_level_token).is_equal(2)
+	
+	## add grass
+	await __wait_to_next_player_turn(IDs.TREEE)
+	await __async_move_mouse_to_cell(Vector2(1,1), true)
+	
+	## check
+	await __wait_to_next_player_turn()
+	
+	assert_array(board.cell_tokens_ids).contains_same_exactly(
+		[
+			[IDs.EMPTY,IDs.EMPTY,IDs.EMPTY],
+			[IDs.EMPTY,IDs.CHE_B,IDs.EMPTY],
+			[IDs.EMPTY,IDs.EMPTY,IDs.EMPTY],
+			[IDs.EMPTY,IDs.EMPTY,IDs.EMPTY]
+		]
+	)
+
+func test__medium_diff_should_use_default_chest_bronze() -> void:
+	
+	
+	var landscape := [
+		[IDs.EMPTY,IDs.EMPTY,IDs.EMPTY],
+		[IDs.EMPTY,IDs.EMPTY,IDs.EMPTY],
+		[IDs.EMPTY,IDs.B_TRE,IDs.EMPTY],
+		[IDs.EMPTY,IDs.B_TRE,IDs.EMPTY]
+	]
+	
+	await __set_to_player_state_with_board(landscape)
+	
+	var diff_points = game_manager.difficulty.total_points
+	game_manager.add_points(diff_points + 10)
+	assert_str(game_manager.difficulty.name).is_equal("Medium")
+	assert_int(game_manager.difficulty.max_level_token).is_equal(3)
+	
+	## add grass
+	await __wait_to_next_player_turn(IDs.B_TRE)
+	await __async_move_mouse_to_cell(Vector2(1,1), true)
+	
+	## check
+	await __wait_to_next_player_turn()
+	
+	assert_array(board.cell_tokens_ids).contains_same_exactly(
+		[
+			[IDs.EMPTY,IDs.EMPTY,IDs.EMPTY],
+			[IDs.EMPTY,IDs.CHE_B,IDs.EMPTY],
+			[IDs.EMPTY,IDs.EMPTY,IDs.EMPTY],
+			[IDs.EMPTY,IDs.EMPTY,IDs.EMPTY]
+		]
+	)
