28dd070ba6afb93e24e24a52808fff47aa2f6c2b
Guille <guillermoprados@users.noreply.github.com>
2023-12-15 09:50:49 -0300

    now clouds move better

--- scenes/menues/main_menu_animator.gd
+++ scenes/menues/main_menu_animator.gd
@@ -2,46 +2,23 @@ extends Node2D
 
 class_name MainMenuBackground
 
+signal  from_sky_animation_finished()
+signal  to_city_animation_finished()
+
 const FROM_SKY_ANIM_TIME = 2
 const TO_CITY_ANIM_TIME = 1
 const SCALE_FACTOR_TO_FRONT = 1.5
 
-@export var menu_mountains:Array[MenuMountain]
-@export var menu_clouds:Array[MenuCloud]
+var background_movables:Array
 
-@export var markers:Array[Marker2D] = []
-
-var markers_original_positions:Array[Vector2] = []
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for marker:Marker2D in markers:
-		markers_original_positions.append(marker.position)
+	background_movables = get_tree().get_nodes_in_group("movable_menu_items")
 	
-	get_viewport().size_changed.connect(__adjust_markers_positions)
-	__adjust_markers_positions()
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta:float) -> void:
-	pass
-	
-	
-func __adjust_markers_positions() -> void:
-	var scale_factor:Vector2 = get_tree().root.content_scale_size
-	var screen_size:Vector2 = get_viewport().get_visible_rect().size
-	
-	# original_pos --> scale_factor
-	# new_pos --> screen_size
-	for i in range(markers.size()):
-		markers[i].position = screen_size * markers_original_positions[i] / scale_factor
-
-
 func animate_from_sky() -> void:
-	for mountain:MenuMountain in menu_mountains:
-		mountain.animate_from_button(FROM_SKY_ANIM_TIME)
-
+	
+	for movable:MenuBackgroundMovable in background_movables:
+		movable.animate_from_button(FROM_SKY_ANIM_TIME)
+	
 func animate_to_city() -> void:
-	for mountain:MenuMountain in menu_mountains:
-		mountain.animate_to_front(SCALE_FACTOR_TO_FRONT, TO_CITY_ANIM_TIME)
-	for cloud:MenuCloud in menu_clouds:
-		cloud.animate_to_front(SCALE_FACTOR_TO_FRONT, TO_CITY_ANIM_TIME)
+	for movable:MenuBackgroundMovable in background_movables:
+		movable.animate_to_front(SCALE_FACTOR_TO_FRONT, TO_CITY_ANIM_TIME)
---
+++ scenes/menues/menu_movable.gd
@@ -0,0 +1,62 @@
+extends Node2D
+
+class_name MenuBackgroundMovable
+
+enum MovableOrientation {
+	CENTER,
+	LEFT,
+	RIGHT,
+}
+
+@export var orientation:MovableOrientation
+
+var normal_position: Vector2
+var bottom_position: Vector2
+var front_position: Vector2
+
+var  animation_tween: Tween
+
+func _ready() -> void:
+	__set_positions()
+		
+func __set_positions() -> void:
+	var screen_size:Vector2 = get_viewport().get_visible_rect().size
+	
+	normal_position = Vector2(screen_size.x / 2 , screen_size.y)
+	front_position = Vector2(screen_size.x / 2 , screen_size.y + (screen_size.y / 10))
+	bottom_position = Vector2(screen_size.x / 2 , screen_size.y + (screen_size.y / 2))
+	
+	var normal_position_x_displacement := screen_size.x/2
+	var front_position_x_displacement := normal_position_x_displacement + screen_size.x/4
+	
+	match  orientation:
+		MovableOrientation.CENTER:
+			pass
+		MovableOrientation.LEFT:
+			normal_position.x = normal_position.x - normal_position_x_displacement
+			front_position.x = normal_position.x - front_position_x_displacement 
+			bottom_position.x = normal_position.x
+		MovableOrientation.RIGHT:
+			normal_position.x = normal_position.x + normal_position_x_displacement
+			front_position.x = normal_position.x + front_position_x_displacement 
+			bottom_position.x = normal_position.x
+			
+	position = normal_position
+	
+func reset_tweeners() -> void:
+	if animation_tween and animation_tween.is_running():
+		animation_tween.kill()
+	animation_tween = create_tween()
+		
+func animate_from_button(time:float) -> void:
+	reset_tweeners()
+	position = bottom_position
+	animation_tween.set_trans(Tween.TRANS_QUAD)
+	animation_tween.set_ease(Tween.EASE_OUT)
+	animation_tween.tween_property(self, "position", normal_position, time)
+	
+func animate_to_front(scale_factor:float, time:float) -> void:
+	reset_tweeners()
+	animation_tween.set_parallel(true)
+	animation_tween.tween_property(self, "position", front_position, time)
+	animation_tween.tween_property(self, "scale", Vector2(scale_factor, scale_factor), time)
