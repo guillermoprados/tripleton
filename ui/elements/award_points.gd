b151c06a963065e5a10974b59d6de53de28a3a5a
Guille <guillermoprados@users.noreply.github.com>
2023-12-18 21:38:36 -0300

    add particles

--- ui/elements/award_points.gd
+++ ui/elements/award_points.gd
@@ -3,28 +3,47 @@ extends Node2D
 class_name AwardPoints
 
 var points_label: RichTextLabel
+var particles: CPUParticles2D
 
 # Duration for which the points should be displayed
 @export var display_duration: float = 1
 @export var move_up_speed : float = 5
 
 var tween:Tween
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	points_label = $Points
-	points_label.text = "0"
 	scale = Vector2.ZERO
+	
+	particles = $Particles
+	points_label.text = "0"
+	particles.set_emitting(false)
+	
+	show_points(50)
+	
 
 func show_points(value: int) -> void:
+	points_label.text = "[tornado radius=5.0 freq=10.0]+"+str(value)
 	tween = create_tween()
-	tween.set_ease(Tween.EASE_IN)
+	tween.set_ease(Tween.EASE_OUT)
 	tween.set_parallel(true)
 	tween.tween_property(self, "scale", Vector2.ONE, display_duration / 4)
 	tween.tween_property(self, "position:y", position.y - 50, display_duration)
-	tween.tween_property(self, "position:y", position.y - 50, display_duration)
-	tween.tween_property(self, "modulate:a", 0, display_duration*1.5)
-	points_label.text = "[tornado radius=5.0 freq=10.0]+"+str(value)
+	tween.tween_property(points_label, "modulate:a", 0, display_duration*1.5)
+	# TODO: use a real reference for position?
+	tween.tween_property(self, "position", Vector2.ZERO, display_duration).set_delay(display_duration)
+	tween.tween_callback(queue_free).set_delay(display_duration*2)
+	
 	$DisplayTimer.start(display_duration)
 	
 func _on_display_timer_timeout() -> void:
-	queue_free()
+	particles.set_emitting(true)
+	
+func _on_points_finished() -> void:
+	tween.kill()
+	tween = create_tween()
+	
+
+
